833090645aaebaa0f9e9d232bd4b42528c6ff6ab
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2023-02-15 14:57:53 +0100

    Created solution for the second exercise.

---
+++ exercises/02-module-class/main.js
@@ -0,0 +1,21 @@
+import ColorGenerator from "./color-generator-2.0.mjs";
+
+const input = document.getElementById('number-input'),
+  requestColorButton = document.getElementById('request-color'),
+  resetButton = document.getElementById('reset'),
+  colorList = document.getElementById('color-list');
+
+const colorGenerator = new ColorGenerator();
+
+function addToList(text) {
+  const listItem = document.createElement('li');
+  listItem.appendChild(document.createTextNode(text));
+  colorList.appendChild(listItem);
+}
+
+requestColorButton.addEventListener('click', () => addToList(colorGenerator.get(input.value)));
+
+resetButton.addEventListener('click', () => {
+  colorGenerator.reset();
+  addToList('reset!');
+});
